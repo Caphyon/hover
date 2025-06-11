c1e6d168cdc2ff9d6cb92e8069eb733e5d23e7f0
Gabriel Diaconita <gabriel.diaconita@caphyon.com>
2025-06-11 16:37:07 +0300

    Embed DLL dependencies into the compiled EXE so that it's usable standalone

--- Hover/Program.cs
+++ Hover/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Reflection;
+using System.Globalization;
 
 namespace Hover
 {
@@ -13,7 +15,27 @@ namespace Hover
     {
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
+      AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
       Application.Run(new LauncherForm());
+    }
+    private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
+    {
+      var executingAssembly = Assembly.GetExecutingAssembly();
+      var assemblyName = new AssemblyName(args.Name);
+
+      var path = assemblyName.Name + ".dll";
+      if (!assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture))
+      {
+        path = $"{assemblyName.CultureInfo}\\${path}";
+      }
+
+      var stream = executingAssembly.GetManifestResourceStream(path);
+      if (stream == null)
+        return null;
+
+      var assemblyRawBytes = new byte[stream.Length];
+      stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+      return Assembly.Load(assemblyRawBytes);
     }
   }
 }
